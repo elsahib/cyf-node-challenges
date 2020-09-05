0a495434b2b26a369222b71a9d861403eb15fc1e
Osman Elsahib <elsahib10@gmail.com>
2020-09-05 17:07:24 +0100

    level 300

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 
 const siteMap = {
-    "/pharmacies": "returns pharmacies list for stratford",
-    "/colleges": "returns colleges list for stratford",
-    "/doctors": "returns doctors list for stratford",
-    "/hospitals": "returns hospitals list for stratford"
+    "/:city/pharmacies": "returns pharmacies list for /:city",
+    "/:city/colleges": "returns colleges list for /:city",
+    "/:city/doctors": "returns doctors list for /:city",
+    "/:city/hospitals": "returns hospitals list for /:city"
 }
 const harrow = require("./data/Harrow.json")
 const stratford = require("./data/Stratford.json")
@@ -19,17 +19,41 @@ const heathrow = require("./data/Heathrow.json")
 app.get("/", function (request, response) {
     response.json(siteMap);
 });
-app.get("/pharmacies", function (request, response) {
-    response.json(stratford.pharmacies);
+app.get("/:city/pharmacies", function (request, response) {
+    if (request.params.city.toLowerCase() === "stratford") {
+        response.json(stratford.pharmacies);
+    } else if (request.params.city.toLowerCase() === "harrow") {
+        response.json(harrow.pharmacies);
+    } else {
+        response.json(heathrow.pharmacies);
+    }
 });
-app.get("/colleges", function (request, response) {
-    response.json(stratford.colleges);
+app.get("/:city/colleges", function (request, response) {
+    if (request.params.city.toLowerCase() === "stratford") {
+        response.json(stratford.colleges);
+    } else if (request.params.city.toLowerCase() === "harrow") {
+        response.json(harrow.colleges);
+    } else {
+        response.json(heathrow.colleges);
+    }
 });
-app.get("/doctors", function (request, response) {
-    response.json(stratford.doctors);
+app.get("/:city/doctors", function (request, response) {
+    if (request.params.city.toLowerCase() === "stratford") {
+        response.json(stratford.doctors);
+    } else if (request.params.city.toLowerCase() === "harrow") {
+        response.json(harrow.doctors);
+    } else {
+        response.json(heathrow.doctors);
+    }
 });
-app.get("/hospitals", function (request, response) {
-    response.json(stratford.hospitals);
+app.get("/:city/hospitals", function (request, response) {
+    if (request.params.city.toLowerCase() === "stratford") {
+        response.json(stratford.hospitals);
+    } else if (request.params.city.toLowerCase() === "harrow") {
+        response.json(harrow.hospitals);
+    } else {
+        response.json(heathrow.hospitals);
+    }
 });
 
 app.listen(port);
